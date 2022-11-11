a97abfaff32de57fd902a1dafd7f4f43febf1cbc
FlorianSymmank <floriansymmank@gmx.de>
2022-11-11 16:28:01 +0100

    next try

--- script.sh
+++ script.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 # updates / installs
-# sudo -i # enter pw j4UvH)kgeD
 sudo apt update
 sudo apt -y upgrade
 sudo apt install curl -y
 sudo apt-get install git -y
 sudo apt-get install postgresql -y
 sudo apt install npm -y
-sudo curl -fsSL https://deb.nodesource.com/setup_16.15.0 | sudo -E bash -
-sudo apt install -y nodejs
-# exit
+sudo curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo dpkg -i --force-overwrite /var/cache/apt/archives/nodejs_16.18.1-deb-1nodesource1_amd64.deb
+sudo apt f- install -y nodejs
 
 # git
-git --version
-cd /home/sqlinject
+# git --version
+cd ~
 git clone https://github.com/FlorianSymmank/SQLInjection.git
 
 # posgresql
 
 # node
+# node --version
 cd ~/SQLInjection/node_js_server
 npm ci
 node app.js # oder npm run start
