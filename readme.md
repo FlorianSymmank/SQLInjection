af485ddfe304ee75683a0aff8ce92e0a644a1676
FlorianSymmank <floriansymmank@gmx.de>
2022-11-11 15:26:10 +0100

    first try at automation

---
+++ readme.md
@@ -0,0 +1,18 @@
+## VirtualBox    
+Neue erstellen  
+Typ: linux  
+Version: Unbuntu 64bit  
+Speichergroeße: 8192  
+Festplatte erzeugen
+Dateigroeße: 15 GB (VDI, dynamisch alloziert)  
+Medium für start -> .iso aus link unten (Ubuntu 22.04.1 LTS)
+
+### Ubuntu 22.04.1 LTS  
+https://ubuntu.com/download/desktop  
+
+### VirtualBox SQLInject Ubuntu  
+name: sqlinject  
+computername: sqlinject-VirtalBox  
+password: j4UvH)kgeD  
+
+### [Einrichten](./script.sh)  aller programme (script.sh)
---
+++ script.sh
@@ -0,0 +1,25 @@
+# updates / installs
+sudo -i # enter pw j4UvH)kgeD
+apt update
+apt -y upgrade
+apt install curl -y
+apt-get install git -y
+apt-get install postgresql -y
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+apt install -y nodejs
+apt install npm -y
+exit
+
+# git
+git --version
+cd /home/sqlinject
+git clone https://github.com/FlorianSymmank/SQLInjection.git
+j4UvH)kgeD
+
+# posgress
+
+# node
+cd ~/SQLInjection/node_js_server
+npm ci
+node app.js # oder npm run start
+# serverlistens on 127.0.0.1:3000
