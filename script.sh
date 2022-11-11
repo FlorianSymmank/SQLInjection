969ca08be9e39f85363e5f81eacd90598ab0f90d
FlorianSymmank <floriansymmank@gmx.de>
2022-11-11 16:03:28 +0100

    second try

--- script.sh
+++ script.sh
@@ -1,22 +1,22 @@
+#!/bin/sh
 # updates / installs
-sudo -i # enter pw j4UvH)kgeD
-apt update
-apt -y upgrade
-apt install curl -y
-apt-get install git -y
-apt-get install postgresql -y
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-apt install -y nodejs
-apt install npm -y
-exit
+# sudo -i # enter pw j4UvH)kgeD
+sudo apt update
+sudo apt -y upgrade
+sudo apt install curl -y
+sudo apt-get install git -y
+sudo apt-get install postgresql -y
+sudo apt install npm -y
+sudo curl -fsSL https://deb.nodesource.com/setup_16.15.0 | sudo -E bash -
+sudo apt install -y nodejs
+# exit
 
 # git
 git --version
 cd /home/sqlinject
 git clone https://github.com/FlorianSymmank/SQLInjection.git
-j4UvH)kgeD
 
-# posgress
+# posgresql
 
 # node
 cd ~/SQLInjection/node_js_server
