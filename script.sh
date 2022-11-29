688ae44966b18c82bb14140b6186b753fbdf423c
FlorianSymmank <floriansymmank@gmx.de>
2022-11-29 20:22:36 +0100

    next try

--- script.sh
+++ script.sh
@@ -21,24 +21,28 @@ cd ~/SQLInjection
 sudo -u postgres -i psql -c "ALTER ROLE postgres WITH PASSWORD 'j4UvH)kgeD';"
 
 # Create DBs
-sudo -u postgres -i createdb hospitalDB
-sudo -u postgres -i createdb canteenDB
+sudo -u postgres -i createdb hospital_db
+sudo -u postgres -i createdb canteen_db
 
 # Create Tables
-sudo -u postgres -i psql hospitalDB < hospital.sql
-sudo -u postgres -i psql canteenDB < canteen.sql
+sudo -u postgres -i psql hospital_db < hospital.sql
+sudo -u postgres -i psql canteen_db < canteen.sql
 
 # Create Users
 sudo -u postgres -i psql -c "CREATE USER hospital WITH ENCRYPTED PASSWORD 'FS5sMhx(MD';"
-sudo -u postgres -i psql -c "GRANT ALL PRIVILEGES ON DATABASE hospitalDB TO hospital;"
-sudo -u postgres -i psql -c "CREATE USER canteen WITH ENCRYPTED PASSWORD 'hosp';"
-sudo -u postgres -i psql -c "GRANT ALL PRIVILEGES ON DATABASE canteenDB TO canteen;"
-sudo -u postgres -i psql -c "GRANT CONNECT ON DATABASE hospitalDB TO canteen;"
-sudo -u postgres -i psql -c "GRANT SELECT ON hospitalDB TO canteen;"
+sudo -u postgres -i psql -c "GRANT CONNECT ON DATABASE hospital_db TO hospital;"
+sudo -u postgres -i psql -d hospital_db -c "GRANT ALL PRIVILEGES ON TABELS IN SCHEMA public TO hospital;"
+
+
+sudo -u postgres -i psql -c "CREATE USER canteen WITH ENCRYPTED PASSWORD 'gNXY=qfn4B';"
+sudo -u postgres -i psql -c "GRANT CONNECT ON DATABASE canteen_db TO canteen;"
+sudo -u postgres -i psql -d canteen_db -c "GRANT ALL PRIVILEGES ON TABELS IN SCHEMA public TO canteen;"
+sudo -u postgres -i psql -c "GRANT CONNECT ON DATABASE hospital_db TO canteen;"
+sudo -u postgres -i psql -d hospital_db -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO canteen;"
 
 # node
 # node --version
 cd ~/SQLInjection/node_js_server
 npm ci
-node app.js # oder npm run start
+nonode app.js &# oder npm run start
 # serverlistens on 127.0.0.1:3000
