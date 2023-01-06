cc816495292cab5c85f3b141ebee5b73e61ef2da
MatthiasWeyirch <s0579274@htw-berlin.de>
2023-01-06 13:48:33 +0100

    change dates and add allergies

--- canteen.sql
+++ canteen.sql
@@ -35,7 +35,6 @@ CREATE TABLE Orders (
     CONSTRAINT Orders_pk PRIMARY KEY (orderId)
 );
 
-<<<<<<< HEAD
 INSERT INTO Dishes (dishId, price, name) VALUES (0, $3.50, "Chicken Noodle Casserole");
 INSERT INTO Dishes (dishId, price, name) VALUES (1, $3.90, "Tuna Patties");
 INSERT INTO Dishes (dishId, price, name) VALUES (2, $3.50, "Quattro Formaggi Pizza");
@@ -86,54 +85,5 @@ INSERT INTO DishAllergen (dishId, allergen) VALUES (12, "peanut");
 INSERT INTO DishAllergen (dishId, allergen) VALUES (13, "lactose");
 INSERT INTO DishAllergen (dishId, allergen) VALUES (14, "egg");
 INSERT INTO DishAllergen (dishId, allergen) VALUES (15, "lactose");
--- End of file.
-=======
-INSERT INTO Dishes (dishId, price, name) VALUES (0, '$3.50', 'Chicken Noodle Casserole');
-INSERT INTO Dishes (dishId, price, name) VALUES (1, '$3.90', 'Tuna Patties');
-INSERT INTO Dishes (dishId, price, name) VALUES (2, '$3.50', 'Quattro Formaggi Pizza');
-INSERT INTO Dishes (dishId, price, name) VALUES (3, '$4.80', 'Veggie Burger');
-INSERT INTO Dishes (dishId, price, name) VALUES (4, '$2.40', 'Biscuits and Gravy');
-INSERT INTO Dishes (dishId, price, name) VALUES (5, '$2.80', 'Bacon and Spinach Pasta');
-INSERT INTO Dishes (dishId, price, name) VALUES (6, '$2.20', 'Chicken Caesar Salad');
-INSERT INTO Dishes (dishId, price, name) VALUES (7, '$5.00', 'Honey Garlic Shrimp');
-INSERT INTO Dishes (dishId, price, name) VALUES (8, '$4.70', 'Honey Mustard Salmon');
-INSERT INTO Dishes (dishId, price, name) VALUES (9, '$3.30', 'Tuna melt Sandwich');
-INSERT INTO Dishes (dishId, price, name) VALUES (10, '$4.90', 'Lemon Butter Shrimp with Asparagus');
-INSERT INTO Dishes (dishId, price, name) VALUES (11, '$2.90', 'Bacon Corn Chowder');
-INSERT INTO Dishes (dishId, price, name) VALUES (12, '$2.30', 'Nachos');
-INSERT INTO Dishes (dishId, price, name) VALUES (13, '$4.00', 'Chicken Cordon Bleu');
-INSERT INTO Dishes (dishId, price, name) VALUES (14, '$2.20', 'Chocolate Cake');
-INSERT INTO Dishes (dishId, price, name) VALUES (15, '$2.20', 'Creme Brulee');
->>>>>>> 535ca0cbba1d339116849f78d3bddbaa0083d7b4
-
-
-INSERT INTO Menu (id, dishId, date, secret) VALUES (0, 0, '2023-01-02', false);
-INSERT INTO Menu (id, dishId, date, secret) VALUES (1, 6, '2023-01-02', false);
-
-INSERT INTO Menu (id, dishId, date, secret) VALUES (2, 1, '2023-01-03', false);
-INSERT INTO Menu (id, dishId, date, secret) VALUES (3, 2, '2023-01-03', false);
-
-INSERT INTO Menu (id, dishId, date, secret) VALUES (4, 12, '2023-01-04', true);
-INSERT INTO Menu (id, dishId, date, secret) VALUES (5, 7, '2023-01-04', false);
-INSERT INTO Menu (id, dishId, date, secret) VALUES (6, 6, '2023-01-04', false);
-
-INSERT INTO Menu (id, dishId, date, secret) VALUES (7, 3, '2023-01-05', false);
-INSERT INTO Menu (id, dishId, date, secret) VALUES (8, 4, '2023-01-05', false);
-
-INSERT INTO Menu (id, dishId, date, secret) VALUES (9, 5, '2023-01-06', false); 
-INSERT INTO Menu (id, dishId, date, secret) VALUES (10, 6, '2023-01-06', false);
-
-INSERT INTO Menu (id, dishId, date, secret) VALUES (11, 9, '2023-01-07', true);
-INSERT INTO Menu (id, dishId, date, secret) VALUES (12, 7, '2023-01-07', false);
-INSERT INTO Menu (id, dishId, date, secret) VALUES (13, 8, '2023-01-07', false);
-
-INSERT INTO Menu (id, dishId, date, secret) VALUES (14, 14, '2023-01-08', true);
-INSERT INTO Menu (id, dishId, date, secret) VALUES (15, 10, '2023-01-08', false);
-INSERT INTO Menu (id, dishId, date, secret) VALUES (16, 11, '2023-01-08', false);
-
-INSERT INTO DishAllergen (dishId, allergen) VALUES (0, 'Chicken');
-INSERT INTO DishAllergen (dishId, allergen) VALUES (0, 'Noodles');
-
-INSERT INTO DishAllergen (dishId, allergen) VALUES (1, 'Tuna');
 
 -- End of file.
