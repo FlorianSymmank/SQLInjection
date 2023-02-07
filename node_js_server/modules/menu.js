1fcce54c85f25583809a71673b49d82a754742f3
David Wolff <s0578072@htw-berlin.de>
2023-02-07 15:01:21 +0100

    Send error messages to frontend

--- node_js_server/modules/menu.js
+++ node_js_server/modules/menu.js
@@ -2,47 +2,57 @@ const { sqlClientPromise } = require("./sql");
 
 const getMenu = async (req, res) => {
 	
-	const date = req.params.date;
-	const patientId = req.params.patientId;
-	
-	const [sqlCanteen, sqlHospital] = await sqlClientPromise;
-	
-	// cross db query to check if patient is allergic to ingredient
-	const result = await sqlHospital.query(`
-		SELECT patientId, allergen FROM PatientAllergen WHERE patientId=${patientId}
-	`);
-	
-	const allergicTo = result.rows;
-	
-	const { rows } = await sqlCanteen.query(`
-		SELECT Dishes.dishId, Dishes.name, Dishes.price FROM Menu, Dishes
-			WHERE Menu.dishId = Dishes.dishId AND Menu.date = '${date}' AND NOT Menu.secret;
-	`);
-	
-	const dishes = await Promise.all(rows.map(async ({ dishid, name, price }) => {
+	try {
 		
-		const { rows } = await sqlCanteen.query(`
-			SELECT allergen FROM DishAllergen
-				WHERE dishId='${dishid}'
+		const date = req.params.date;
+		const patientId = req.params.patientId;
+		
+		const [sqlCanteen, sqlHospital] = await sqlClientPromise;
+		
+		// cross db query to check if patient is allergic to ingredient
+		const result = await sqlHospital.query(`
+			SELECT patientId, allergen FROM PatientAllergen WHERE patientId=${patientId}
 		`);
 		
-		const allergens = rows.map(({ allergen }) => allergen);
-		const allergicReaction = allergicTo.some(v => allergens.includes(v.allergen));
+		const allergicTo = result.rows;
 		
-		return {
-			dishid,
-			name,
-			price,
-			allergens,
-			allergicReaction
-		};
+		const { rows } = await sqlCanteen.query(`
+			SELECT Dishes.dishId, Dishes.name, Dishes.price FROM Menu, Dishes
+				WHERE Menu.dishId = Dishes.dishId AND Menu.date = '${date}' AND NOT Menu.secret;
+		`);
 		
-	}));
-	
-	res.json({
-		dishes
-	});
-	res.end();
+		const dishes = await Promise.all(rows.map(async ({ dishid, name, price }) => {
+			
+			const { rows } = await sqlCanteen.query(`
+				SELECT allergen FROM DishAllergen
+					WHERE dishId='${dishid}'
+			`);
+			
+			const allergens = rows.map(({ allergen }) => allergen);
+			const allergicReaction = allergicTo.some(v => allergens.includes(v.allergen));
+			
+			return {
+				dishid,
+				name,
+				price,
+				allergens,
+				allergicReaction
+			};
+			
+		}));
+		
+		res.json({
+			dishes
+		});
+		res.end();
+		
+	} catch (error) {
+		
+		res.status(500);
+		res.json(error);
+		res.end();
+		
+	}
 	
 };
 
--- node_js_server/routes/apiRouter.js
+++ node_js_server/routes/apiRouter.js
@@ -3,6 +3,6 @@ const menu = require(global.appRoot + "/modules/menu");
 
 const router = express.Router();
 
-router.get("/menu/:date/:patientId", menu.getMenu);
+router.get("/menu/:date", menu.getMenu);
 
 module.exports = router;
