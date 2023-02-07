913f55c729ff6bee7e6eb6ae0984818c72e8b5aa
David Wolff <s0578072@htw-berlin.de>
2023-02-07 14:37:34 +0100

    Add separate login page

--- sqli-frontend/src/MainContent.js
+++ sqli-frontend/src/MainContent.js
@@ -2,41 +2,28 @@ import './MainContent.css';
 import { useState } from 'react';
 import Menu from './Menu';
 
-const MainContent = () => {
-
+const MainContent = ({patientName}) => {
+	
 	const [date, setDate] = useState(new Date());
-	const [patientId, setPatientId] = useState(0);
-
+	
 	const handleDateChange = event => {
-
+		
 		if (event.target.value === '')
 			return;
+		
 		setDate(new Date(event.target.value));
-
+		
 	};
-
-	const handlePatientIdChange = event => {
-
-		if (event.target.value === '')
-			return;
-
-		setPatientId(event.target.value);
-
-	};
-
+	
 	const dateToString = date => date.toISOString().split('T')[0];
-
+	
 	return (
 		<div className="MainContent">
 			<div>
 				<span>Canteen Menu for {date.toDateString()}</span>
 				<input type="date" value={dateToString(date)} onChange={handleDateChange} />
 			</div>
-			<div>
-				<span>Patient: <input type="number" value={patientId} onChange={handlePatientIdChange} /></span>
-
-			</div>
-			<Menu date={dateToString(date)} patientId={patientId} />
+			<Menu date={dateToString(date)} patientName={patientName} />
 		</div>
 	);
 }
