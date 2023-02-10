92960866fa24882500f3cb74b98601dd211b14b9
FlorianSymmank <floriansymmank@gmx.de>
2023-02-10 13:20:42 +0100

    simple formatting

--- sqli-frontend/src/Login.js
+++ sqli-frontend/src/Login.js
@@ -1,52 +1,63 @@
-import {useState} from 'react';
+import './Login.css';
+import { useState } from 'react';
+import { apiCall } from './util';
 
-const Login = ({setLoginData}) => {
-	
+const Login = ({ setLoginData }) => {
+
 	const [name, setName] = useState('');
 	const [password, setPassword] = useState('');
-	
+	const [loginFailed, setLoginFailed] = useState(false);
+
 	const handleNameChange = event => {
-		
 		setName(event.target.value);
-		
 	};
-	
+
 	const handlePasswordChange = event => {
-		
 		setPassword(event.target.value);
-		
 	};
-	
+
 	const login = async () => {
-		
+
 		const loginData = {
 			name,
 			password,
 			isGuest: false
 		};
-		
-		setLoginData(loginData);
-		
+
+		try {
+			// test if credentials are correct, throws if not
+			await apiCall(`menu/2023-01-01`, loginData);
+			setLoginData(loginData);
+			setLoginFailed(false)
+		} catch (e) {
+			setLoginFailed(true)
+		}
 	};
-	
+
 	const skip = () => {
-		
+
 		setLoginData({
 			isGuest: true
 		});
-		
+
 	};
-	
+
 	return <>
-		<h2>Login</h2>
-		Enter name to login:
-		<input value={name} onChange={handleNameChange} />
-		Enter password:
-		<input value={password} onChange={handlePasswordChange} />
-		<button onClick={login}>Login</button>
-		<button onClick={skip}>Continue as guest</button>
+		<div>
+			<h2>Login</h2>
+			<input value={name} onChange={handleNameChange} placeholder='Name' />
+			<br />
+			<input value={password} onChange={handlePasswordChange} type="password" placeholder='Password' />
+			<br />
+			<br />
+			<button onClick={login}>Login</button>
+			<button onClick={skip}>Continue as guest</button>
+			<br />
+			<br />
+			<div className={loginFailed ? 'error-message' : 'error-message hidden'}>The credentials you provided are incorrect.</div>
+		</div>
 	</>;
-	
+
 };
 
 export default Login;
